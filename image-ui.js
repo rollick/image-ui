18964f000c4c43958bb6c9ba031fd83f9bc694f2
rollick <mark@onepercentclub.com>
2014-11-08 23:31:43 +0100

    add styling and fix serving of images

--- image-ui.js
+++ image-ui.js
@@ -1,15 +1,35 @@
 Images = new Mongo.Collection("images");
 
 if (Meteor.isServer) {
+  // Note: for development we need a separate uploads directory for the 
+  //       servering the images. Production should use nginx etc.
+  var fs = Npm.require('fs');
+  WebApp.connectHandlers.use(function(req, res, next) {
+      var re = /^\/uploads\/(.*)$/.exec(req.url);
+
+      // Only handle URLs that start with /uploads/*
+      if (re !== null) {   
+          var filePath = process.env.PWD + '/.uploads/' + re[1];
+          var data = fs.readFileSync(filePath, data);
+          res.writeHead(200, {
+            'Content-Type': 'image'
+          });
+
+          res.write(data);
+          res.end();
+      } else {  
+          // Other urls will have default behaviors
+          next();
+      }
+  });
+
   Meteor.publish("images", function (limit) {
     if (typeof limit == 'undefined')
       limit = 99;
 
-    return Images.find({}, {limit: limit, sort: {dateTaken: -1}});
-  });
+    console.log('Publishing images...')
 
-  Meteor.startup(function () {
-    // code to run on server at startup
+    return Images.find();
   });
 }
 
@@ -18,13 +38,17 @@ if (Meteor.isClient) {
 
   Template.gallery.helpers({
     images: function () {
-      return Images.find();
+      return Images.find({}, {sort: {dateTaken: 1}}).fetch();
     }
   });
 
+  Template.gallery.rendered = function() {
+    AnimatedEach.attachHooks(this.find(".links"));
+  };
+
   Template.image.helpers({
     imagePath: function () {
-      return "/images" + this.path.match(/\/Users\/onepercentclub\/Pictures\/Trip(.*?)$/)[1];
+      return "/uploads/images" + this.path.match(/\/Users\/onepercentclub\/Pictures\/Trip(.*?)$/)[1];
     }
   });
 
@@ -40,5 +64,5 @@ if (Meteor.isClient) {
 
       blueimp.Gallery(links, options);
     }
-  })
+  });
 }
