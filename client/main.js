dcca16c55575a10e1a4b97692cb4deaa1025c43b
rollick <mark@onepercentclub.com>
2015-03-02 09:45:59 +0100

    try to contain the gallery links within visible display

--- client/main.js
+++ client/main.js
@@ -7,33 +7,6 @@ Session.setDefault('galleryId', null);
 window.gallery = gallery = null;
 answerTimer = questionTimer = null;
 
-var min_x = 0,
-    max_x = 0,
-    min_y = 0,
-    max_y = 0,
-    filled_areas = new Array();
-
-function check_overlap(area) {
-    for (var i = 0; i < filled_areas.length; i++) {
-        check_area = filled_areas[i];
-        
-        var bottom1 = area.y + area.height;
-            bottom2 = check_area.y + check_area.height,
-            top1 = area.y,
-            top2 = check_area.y,
-            left1 = area.x,
-            left2 = check_area.x,
-            right1 = area.x + area.width,
-            right2 = check_area.x + check_area.width;
-
-        if (bottom1 < top2 || top1 > bottom2 || right1 < left2 || left1 > right2) {
-            continue;
-        }
-        return true;
-    }
-    return false;
-}
-
 Meteor.startup(function () {
     Session.setDefault('sortDir', 1);
     Session.set('clientId', Math.random().toString(36).slice(2));
@@ -179,16 +152,46 @@ Template.Home.helpers({
     }
 });
 
+var padding = 15,
+    min_x = padding,
+    max_x = 0,
+    min_y = padding,
+    max_y = 0,
+    filled_areas = new Array();
+
+function check_overlap(area) {
+    for (var i = 0; i < filled_areas.length; i++) {
+        check_area = filled_areas[i];
+        
+        var bottom1 = area.y + area.height;
+            bottom2 = check_area.y + check_area.height,
+            top1 = area.y,
+            top2 = check_area.y,
+            left1 = area.x,
+            left2 = check_area.x,
+            right1 = area.x + area.width,
+            right2 = check_area.x + check_area.width;
+
+        if (bottom1 < top2 || top1 > bottom2 || right1 < left2 || left1 > right2) {
+            continue;
+        }
+        return true;
+    }
+    return false;
+}
+
 Template.GalleryLink.rendered = function() {
     var element = $(this.firstNode),
         rand_x = 0,
         rand_y = 0,
+        width = element.width(),
+        height = element.height(),
         area;
 
     do {
-        rand_x = Math.round(min_x + ((max_x - min_x)*(Math.random() % 1)));
-        rand_y = Math.round(min_y + ((max_y - min_y)*(Math.random() % 1)));
-        area = {x: rand_x, y: rand_y, width: element.width(), height: element.height()};
+        rand_x = Math.round(min_x + ((max_x - min_x - width - padding)*(Math.random() % 1)));
+        rand_y = Math.round(min_y + ((max_y - min_y - height - padding)*(Math.random() % 1)));
+        area = {x: rand_x, y: rand_y, width: width, height: height};
     } while(check_overlap(area));
     
     filled_areas.push(area);
