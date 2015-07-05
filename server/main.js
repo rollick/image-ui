46f8feda2ea6f22a9924ade3f39ca3b0ab67224f
rollick <mark@onepercentclub.com>
2015-07-05 16:18:14 +0200

    handle filtering images by tags

--- server/main.js
+++ server/main.js
@@ -31,17 +31,54 @@ Meteor.publish("images", function (options) {
     }
 
     console.log('Publishing images...');
-    return Images.find({gallery_id: options.galleryId}, {
-        fields: {
-            _id: 1, 
-            date_taken: 1,
-            hash: 1,
-            relative_path: 1,
-            exif: 1,
-            gallery_id: 1,
-            tags: 1
+
+    var fields = {
+        _id: 1, 
+        date_taken: 1,
+        hash: 1,
+        relative_path: 1,
+        exif: 1,
+        gallery_id: 1,
+        tags: 1
+    };
+
+    // Images can be filtered by one or more sets of 
+    // tags. eg:
+    // tags = ['red', 'blue'] or
+    // tags = [['red', 'blue'], ['green', 'brown']]
+    var tags = options.tags;
+    if (_.isArray(tags) && ! _.isEmpty(tags)) {
+        var tagsQuery = {$or: []};
+
+        // Test if a single set of tags or multiple
+        if (_.isArray(tags[0])) {
+            // Return an array of image cursors based on 
+            // each set of tags.
+            _.each(tags, function (tagSet) {
+                tagsQuery['$or'].push({tags: {$all: tagSet}});      
+            });
+        } else {
+            tagsQuery['$or'].push({tags: {$all: tags}});
         }
-    });
+
+        // Return a image cursor based on tags.
+        console.log(JSON.stringify(tagsQuery));
+
+        return Images.find({
+            $and: [
+                {gallery_id: options.galleryId},
+                tagsQuery
+            ]
+        }, {
+            fields: fields
+        });
+    } else {
+        // Return a image cursor based without tags query.
+        return Images.find({gallery_id: options.galleryId}, {
+            fields: fields
+        });  
+    }
+
 });
 
 Meteor.publish("slideshow", function () {
